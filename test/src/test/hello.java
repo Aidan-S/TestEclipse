b5554316403caf8806cc38f979ad22090b27a158
gabbley <baniquedg@bishopireton.org>
2017-09-08 11:42:35 -0400

    gabby's changes

--- test/src/test/hello.java
+++ test/src/test/hello.java
@@ -1,6 +1,6 @@
 package test;
 import java.util.Scanner;
-
+//gabby
 public class hello {
 	
 	public static void main(String args[]){
@@ -14,7 +14,7 @@ public class hello {
 	      System.out.println("Dif = " + dif(a,b));
 	      System.out.println("Div = " + div(a,b));
 	      System.out.println("Mult = " + mult(a,b));
-	      
+	      System.out.println("Mod = " + mod(a,b));
 	}
 	
 	public static double sum (double a, double b){
@@ -32,5 +32,11 @@ public class hello {
 	public static double mult (double a, double b){
 		return a * b;
 	}
+	
+	//gabby added mod method
+	public static double mod (double a, double b) {
+		return a % b;
+	}
+	
 
 }
